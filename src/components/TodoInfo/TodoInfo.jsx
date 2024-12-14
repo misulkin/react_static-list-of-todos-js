ce87ed770748eee7e7f8081c1c8d657df9799633
Artur Misko <arthurmisko@gmail.com>
2024-12-14 20:14:18 +0100

    didntpasstest

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,11 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ task }) => (
+  <article
+    className={task.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+  >
+    <h2 className="TodoInfo__title">{task.title}</h2>
+
+    <UserInfo task={task} />
+  </article>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(task => (
+      <TodoInfo task={task} />
+    ))}
+  </section>
+);
